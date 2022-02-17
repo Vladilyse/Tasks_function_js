e3c95fee6a4954d75cb53470184c70dd2824a14d
Vladilyse <92036521+Vladilyse@users.noreply.github.com>
2022-02-17 17:34:29 +0200

    Tasks_funct_JS

---
+++ main.js
@@ -0,0 +1,41 @@
+// Function1
+// function display_message() {
+//     let a = `Спать меньше 6 часов в сутки - вредно для вашего здоровья`
+//     alert(a)
+// }
+// display_message()
+
+// Function2
+// function favourite_book(title) {
+//     let k = `One of my favourite books is ${title}`
+//     alert(k)
+// }
+// let a = prompt("Давай вводи свою любимую книгу")
+// favourite_book(a)
+
+// Function3
+// function make_shirt(size,text) {
+//     let k = `Размер футболки: ${size}, текст на футболке: ${text}`
+//     alert(k)
+// }
+// let a = +prompt("Введите размер футболки")
+//     b = prompt("Введите текст на футболке")
+// make_shirt(a,b)
+
+// Function4
+// function make_shirt(size,text) {
+//     let k = `Размер футболки: ${size}, текст на футболке: ${text}`
+//     alert(k)
+// }
+// let a = "L"
+//     b = "I Love JS"
+// make_shirt(a,b)
+
+// Function5
+// function describe_city(city, country) {
+//     let k = `${city} is in ${country}`
+//     alert(k)
+// }
+// let city = prompt("Введи название своего города")
+//     country = prompt("Введите название своей страны")
+// describe_city(city, country)
